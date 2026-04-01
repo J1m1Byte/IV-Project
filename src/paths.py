cfde7fae67a36eca0d96f7890bc983b01eea85f7
J1m1Byte <aamirrezajamshidi@gmail.com>
2026-04-01 18:56:25 -0400

    new data batch

--- src/paths.py
+++ src/paths.py
@@ -7,6 +7,7 @@ DATA = ROOT / 'data'
 RAW_DATA = DATA / 'raw'
 INTERIM_DATA = DATA / 'interim'
 CLEAN_DATA = DATA / 'clean'
+CLEAN_DATA_V2 = CLEAN_DATA / 'v2'
 
 OUTPUT = ROOT / 'output'
 FIG_OUTPUT = OUTPUT / 'fig'
